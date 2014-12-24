c68608d72c41dcaa0e1461fd7edb06757956175f
cooltoast <mpatel678@gmail.com>
2014-12-23 21:28:37 -0500

    basic form to add disease to index.html

--- django/medicalCosts/diseases/views.py
+++ django/medicalCosts/diseases/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from diseases.models import Operation, Procedure, Disease
+from django.core.urlresolvers import reverse
 
 def index(request):
   return render(request, 'diseases/index.html', { 'diseases': Disease.objects.order_by('name') })
@@ -8,6 +9,25 @@ def index(request):
 def disease(request, disease_id):
   disease = get_object_or_404(Disease, pk=disease_id)
   return render(request, 'diseases/disease.html', {'disease': disease})
+
+def add_disease(request):
+  try:
+    d = Disease.objects.get(name__iexact=request.POST['name'])
+  except (KeyError):
+    return render(request,
+      'diseases/index.html',
+      { 'diseases': Disease.objects.order_by('name'),
+      'error_message': "Please enter a disease name." })
+  except (Disease.DoesNotExist):
+    d = Disease(name=request.POST['name'])
+    d.save()
+    return HttpResponseRedirect(reverse('diseases:disease', args=(d.id,)))
+  else:
+    return render(request,
+      'diseases/index.html',
+      { 'diseases': Disease.objects.order_by('name'),
+      'error_message': "Please enter a unique disease name" })
+
 
 def procedure(request, procedure_id):
   procedure = get_object_or_404(Procedure, pk=procedure_id)
