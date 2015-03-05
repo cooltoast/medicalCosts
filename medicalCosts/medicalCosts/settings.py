9351d08d9be36782be8243273d2521b64c8f5e1c
cooltoast <mpatel678@gmail.com>
2015-03-04 23:29:22 -0800

    urls and root views for REST API

--- medicalCosts/medicalCosts/settings.py
+++ medicalCosts/medicalCosts/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diseases',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -90,3 +91,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST API Framework
+# http://www.django-rest-framework.org/
+
+"""
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+"""
+
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+  'PAGINATE_BY': 10
+}
--- medicalCosts/medicalCosts/urls.py
+++ medicalCosts/medicalCosts/urls.py
@@ -1,11 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from rest_framework import routers
+from diseases import views
+
+
+router = routers.DefaultRouter()
+router.register(r'diseases', views.DiseaseViewSet)
+router.register(r'procedures', views.ProcedureViewSet)
+router.register(r'operations', views.OperationViewSet)
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'medicalCosts.views.home', name='home'),
+    url(r'^', include(router.urls)),
+    #url(r'^$', 'medicalCosts.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^diseases/', include('diseases.urls', namespace='diseases')),
 )
